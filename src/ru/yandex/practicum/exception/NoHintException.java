d7ffcbbdb2b3486727d5085ae9a47ddfba28ef19
13rattlehead <rattlehead13@yandex.ru>
2026-03-26 15:20:43 +0300

    исправлен CodeStyle NoHintException

--- src/ru/yandex/practicum/exception/NoHintException.java
+++ src/ru/yandex/practicum/exception/NoHintException.java
@@ -1,9 +1,8 @@
 package ru.yandex.practicum.exception;
 
 public class NoHintException extends Exception {
-    public NoHintException() {
-        super();
-    }
+    public NoHintException() { super(); }
+
     public NoHintException(String message) {
         super("Нет доступных подсказок: " + message);
     }
